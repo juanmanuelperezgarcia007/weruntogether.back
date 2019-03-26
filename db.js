830dd1821a6507718574a1eb141ad3c5e28d7b0e
jymmyneutron <jymmyneutron007@gmail.com>
2019-03-26 17:07:21 +0100

    db perfect

--- db.js
+++ db.js
@@ -2,16 +2,16 @@ const mysql = require('mysql')  //Siempre suele ser igual
 
 let pool = null
 
-let connect= (done) => {
-    pool= mysql.createPool({
-        host:'127.0.0.1',
-        user: 'root',
-        password: '',
-        port: 3306,
-        database:'weruntogether'
-    })
-    done()
-}
+// let connect= (done) => {
+//     pool= mysql.createPool({
+//         host:'127.0.0.1',
+//         user: 'root',
+//         password: '',
+//         port: 3306,
+//         database:'weruntogether'
+//     })
+//     done()
+// }
 
 let connect= (done) => {
     pool= mysql.createPool({

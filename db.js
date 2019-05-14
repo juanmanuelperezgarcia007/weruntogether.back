3fef5af2e843551eb59cb38df12101a5a78e1fcd
jymmyneutron <jymmyneutron007@gmail.com>
2019-05-14 22:45:10 +0200

    Version V.3 (online)

--- db.js
+++ db.js
@@ -2,27 +2,27 @@ const mysql = require('mysql')  //Siempre suele ser igual
 
 let pool = null
 
-let connect= (done) => {
-    pool= mysql.createPool({
-        host:'127.0.0.1',
-        user: 'root',
-        password: '',
-        port: 3306,
-        database:'weruntogether'
-    })
-    done()
-}
-
 // let connect= (done) => {
 //     pool= mysql.createPool({
-//         host:'localhost',
-//         user: 'weruntog_admin',
-//         password: 'Erika007',
+//         host:'127.0.0.1',
+//         user: 'root',
+//         password: '',
 //         port: 3306,
-//         database:'weruntog_produccion'
+//         database:'weruntogether'
 //     })
 //     done()
 // }
+
+let connect= (done) => {
+    pool= mysql.createPool({
+        host:'localhost',
+        user: 'weruntog_admin',
+        password: 'Erika007',
+        port: 3306,
+        database:'weruntog_produccion'
+    })
+    done()
+}
 
 let get = () =>{
     return pool
